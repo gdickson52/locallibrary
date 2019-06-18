cd597dcae781c903ff0ec3d0a23692d2afe9f36c
gdickson52 <gdickson52@gmail.com>
2019-06-18 17:53:05 -0400

    chapters 5-6

---
+++ catalog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+]
+
+
