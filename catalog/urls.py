e0cf0269f884a6bfe38f90e1436a9f13b7e4e16f
gdickson52 <gdickson52@gmail.com>
2019-06-19 17:59:32 -0400

    chapters 6-9

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,6 +12,13 @@ urlpatterns = [
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+urlpatterns += [   
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+
 
 
